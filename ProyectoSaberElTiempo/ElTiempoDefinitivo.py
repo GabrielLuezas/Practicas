9a1da18b59986d90cb464879b4aca8c1486e2dff
GabrielLuezas <gabrieluezas@gmail.com>
2023-04-19 11:57:19 +0200

    Update ElTiempoDefinitivo.py

--- ProyectoSaberElTiempo/ElTiempoDefinitivo.py
+++ ProyectoSaberElTiempo/ElTiempoDefinitivo.py
@@ -3,6 +3,31 @@ from codaio import Coda, Document, Cell, Row
 import requests
 import datetime
 
+#Actualizar fila de coda
+def actualizar():
+    coda = Coda('c5149980-deb1-4979-952f-f3d49ec5b038')
+    doc = Document('05ZbXhV8oe', coda=coda)
+    table = doc.get_table("Temperaturas")
+    celda1 = Cell(column='c-77h06AtH4R', value_storage= location)
+    celda2 = Cell(column='c-b_7VlkcHlZ', value_storage= description)
+    celda3 = Cell(column='c-orQl3rYoNT', value_storage= temperature)
+    celda4 = Cell(column='c-oIsDqmPb5H', value_storage= horaformato)
+    celda5 = Cell(column='c-unFl3Gz49K', value_storage= fechaformato)
+    table.update_row(id_fila, [celda2, celda3, celda4, celda5])
+    print (f"La temperatura de {location} ha sido actualizada correctamente")
+#Insertar nueva fila en coda
+def introducir():
+    coda = Coda('c5149980-deb1-4979-952f-f3d49ec5b038')
+    doc = Document('05ZbXhV8oe', coda=coda)
+    table = doc.get_table("Temperaturas")
+    celda1 = Cell(column='c-77h06AtH4R', value_storage= location)
+    celda2 = Cell(column='c-b_7VlkcHlZ', value_storage= description)
+    celda3 = Cell(column='c-orQl3rYoNT', value_storage= temperature)
+    celda4 = Cell(column='c-oIsDqmPb5H', value_storage= horaformato)
+    celda5 = Cell(column='c-unFl3Gz49K', value_storage= fechaformato)
+    table.upsert_rows([[celda1, celda2, celda3, celda4, celda5]])
+    print (f"La temperatura de {location} ha sido añadida correctamente")
+
 coda = Coda('c5149980-deb1-4979-952f-f3d49ec5b038')
 doc = Document('05ZbXhV8oe', coda=coda)
 table = doc.get_table("Temperaturas")
@@ -77,41 +102,21 @@ if ciudad in ciudades:
     print("La ciudad introducida ya esta en ciudades, voy a actualizar la fila con los nuevos valores")
     print(f'El clima actual en {location} es {description} con una temperatura de {temperature} grados Celsius a las {horaformato} el {fechaformato}')
     
+    #Consultamos a coda la informacion de nuestras filas
     headers = {'Authorization': 'Bearer c5149980-deb1-4979-952f-f3d49ec5b038'}
     uri = f'https://coda.io/apis/v1/docs/05ZbXhV8oe/tables/grid-IOJFdjLyhy/rows'
     res = requests.get(uri, headers=headers)
     informacion = res.json()
+    #Hacemos un bucle, que busca en items si algun item tiene el valor de ciudad, y si lo tiene que nos de la id y salir del bucle
     for item in informacion['items']:
         if item['name'] == ciudad:
             id_fila = item['id']
             break
     print(f"El id de fila de la ciudad {ciudad} es {id_fila}")
-
-
-    #Parte de Coda
-    coda = Coda('c5149980-deb1-4979-952f-f3d49ec5b038')
-    doc = Document('05ZbXhV8oe', coda=coda)
-    table = doc.get_table("Temperaturas")
-    celda1 = Cell(column='c-77h06AtH4R', value_storage= location)
-    celda2 = Cell(column='c-b_7VlkcHlZ', value_storage= description)
-    celda3 = Cell(column='c-orQl3rYoNT', value_storage= temperature)
-    celda4 = Cell(column='c-oIsDqmPb5H', value_storage= horaformato)
-    celda5 = Cell(column='c-unFl3Gz49K', value_storage= fechaformato)
-    table.update_row(id_fila, [celda2, celda3, celda4, celda5])
-    print (f"La temperatura de {location} ha sido actualizada correctamente")
+    actualizar() 
 
     
 else:
     print("La ciudad introducida no esta en ciudades")
     print(f'El clima actual en {location} es {description} con una temperatura de {temperature} grados Celsius a las {horaformato} el {fechaformato}')
-    #Parte de Coda
-    coda = Coda('c5149980-deb1-4979-952f-f3d49ec5b038')
-    doc = Document('05ZbXhV8oe', coda=coda)
-    table = doc.get_table("Temperaturas")
-    celda1 = Cell(column='c-77h06AtH4R', value_storage= location)
-    celda2 = Cell(column='c-b_7VlkcHlZ', value_storage= description)
-    celda3 = Cell(column='c-orQl3rYoNT', value_storage= temperature)
-    celda4 = Cell(column='c-oIsDqmPb5H', value_storage= horaformato)
-    celda5 = Cell(column='c-unFl3Gz49K', value_storage= fechaformato)
-    table.upsert_rows([[celda1, celda2, celda3, celda4, celda5]])
-    print (f"La temperatura de {location} ha sido añadida correctamente")
+    introducir()
