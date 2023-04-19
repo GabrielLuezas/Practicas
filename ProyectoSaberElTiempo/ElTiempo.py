662a4433815106368b3917aa065513c1607dd405
GabrielLuezas <gabrieluezas@gmail.com>
2023-04-19 17:31:35 +0200

    Update ElTiempo.py

--- ProyectoSaberElTiempo/ElTiempo.py
+++ ProyectoSaberElTiempo/ElTiempo.py
@@ -4,7 +4,7 @@ import datetime
 #Pregunta que te hace decir una ciudad
 ciudad = input("Dime una Ciudad: ")
 params = {
-  'access_key': '9fb25c002a54d299b97223b9503828a0',
+  'access_key': '915c29147962800727053090855eed9f',
   'query': ciudad
 }
 #Get para conseguir informacion sobre el tiempo
