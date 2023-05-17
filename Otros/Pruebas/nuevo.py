5d3dddf08cbf2728e94a3a0d901d959b48d7ab3d
GabrielLuezas <gabrieluezas@gmail.com>
2023-05-17 08:48:16 +0200

    Update nuevo.py

--- Otros/Pruebas/nuevo.py
+++ Otros/Pruebas/nuevo.py
@@ -2,30 +2,29 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
-root = tk.Tk()
-root.geometry("350x100")
+ventana = tk.Tk()
+ventana.geometry("350x100")
 
 style = ttk.Style()
 style.theme_use('default')
 style.configure("blue.Horizontal.TProgressbar", background='pink', troughcolor='white')
 
-progress = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate", style="blue.Horizontal.TProgressbar")
+progress = ttk.Progressbar(ventana, orient="horizontal", length=200, mode="determinate", style="blue.Horizontal.TProgressbar")
 progress.pack(pady=10)
 
 def update_progress():
-    progress["value"] += 10
+    progress["value"] += 25
     if progress["value"] < 100:
-        root.after(1000, update_progress)
+        ventana.after(1000, update_progress)
     else:
         messagebox.showinfo("Mensaje de alerta", "¡Proceso completado con éxito!")
 
 def reset_progress():
     progress["value"] = 0
 
-start_button = tk.Button(root, text="Comenzar", command=update_progress)
+start_button = tk.Button(ventana, text="Comenzar", command=update_progress)
 start_button.pack(side="left", padx=10)
 
-reset_button = tk.Button(root, text="Reiniciar", command=reset_progress)
+reset_button = tk.Button(ventana, text="Reiniciar", command=reset_progress)
 reset_button.pack(side="left", padx=10)
-
-root.mainloop()
+ventana.mainloop()
