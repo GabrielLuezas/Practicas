0fddc121bef6cd319a6588e8ed6d908155b7eacf
GabrielLuezas <gabrieluezas@gmail.com>
2023-06-21 08:29:24 +0200

    Create Leccion2.py

---
+++ W3School/Leccion2.py
@@ -0,0 +1,18 @@
+frutas = ["manzana", "platano", "mandarina"]
+i = 0
+while i < len(frutas):
+  print(frutas[i])
+  i = i + 1
+
+prueba3 = {
+  "Tipo": "Planta",
+  "Generacion": "Cuarta",
+  "Nombre": "Turtwig"
+}
+x = prueba3["Tipo"]
+print(x)
+
+def my_function():
+  print("Esto es mi funcion")
+
+my_function()
